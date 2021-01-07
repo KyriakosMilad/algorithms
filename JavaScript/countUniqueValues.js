6a5de75e9880290475cb78cc7391159d4ad127f9
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-01-07 09:01:02 +0200

    Solve countUniqueValues.js

--- JavaScript/countUniqueValues.js
+++ JavaScript/countUniqueValues.js
@@ -3,4 +3,17 @@
  * @param n: array
  * @return integer
  */
-function countUniqueValues(array) {}
+function countUniqueValues(array) {
+	let values = array;
+	let uniqueValues = [];
+
+	for (let val of values) {
+		if (uniqueValues.indexOf(val) === -1) {
+			uniqueValues.push(val);
+		}
+	}
+
+	return uniqueValues.length;
+}
+
+console.log(countUniqueValues('a7a'));
