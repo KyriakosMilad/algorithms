2072617d414d418d296d2017af58e825733268fc
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-01-07 07:33:30 +0200

    Update fizzBuzz.js description

--- JavaScript/fizzBuzz.js
+++ JavaScript/fizzBuzz.js
@@ -1,5 +1,5 @@
 /**
- * @desc loop from 0 to (n) and check numbers between if dividable by 5 print Buzz and if dividable by 3 print Fizz.
+ * @desc loop from 0 to (n) and check numbers between if dividable by 5 print Buzz and if dividable by 3 print Fizz and if dividable by both print FizzBuzz.
  * @param n: integer
  * @return void
  */
