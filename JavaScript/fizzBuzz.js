c39f25ce429ec30d1b40e5dba74e2a4aca1a1741
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-01-07 07:28:46 +0200

    Create fizzBuzz.js

---
+++ JavaScript/fizzBuzz.js
@@ -0,0 +1,6 @@
+/**
+ * @desc loop from 0 to (n) and check numbers between if dividable by 5 print Buzz and if dividable by 3 print Fizz.
+ * @param n: integer
+ * @return void
+ */
+function fizzBuzz(n) {}
