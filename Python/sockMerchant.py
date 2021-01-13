03ee7ec973d8a9b71f5bc63d27b71cb061bf4fb1
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-01-13 17:02:29 +0200

    Solve sockMerchant.py

--- Python/sockMerchant.py
+++ Python/sockMerchant.py
@@ -1,2 +1,28 @@
 # challenge from hackerrank https://www.hackerrank.com/challenges/sock-merchant
 
+def int_list(item):
+    return int(item)
+
+
+socks = map(int_list, input('Enter socks each sock separated with ",": ').split(','))
+socks = list(socks)
+
+colors_pairs = {}
+total_pairs = 0
+
+for i in socks:
+    if i in colors_pairs:
+        colors_pairs[i] += 1
+    else:
+        colors_pairs[i] = 1
+
+print(colors_pairs)
+
+for i in colors_pairs:
+    if colors_pairs[i] % 2 == 0:
+        total_pairs += colors_pairs[i] / 2
+    else:
+        total_pairs += (colors_pairs[i] - 1) / 2
+    print(i, total_pairs)
+print(int(total_pairs))
+
