112dda4e33ce20eff6b4e0a83b985b8b591b98bd
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-01-13 15:34:14 +0200

    Solve catsAndMouse.py

--- Python/catsAndMouse.py
+++ Python/catsAndMouse.py
@@ -1,1 +1,12 @@
 # challenge from hackerrank https://www.hackerrank.com/challenges/ctci-making-anagrams
+
+a = int(input('first cat position: '))
+b = int(input('second cat position: '))
+c = int(input('mouse position: '))
+
+if abs(c-b) > abs(c-a):
+    print('Cat A')
+elif abs(c-a) > abs(c-b):
+    print('Cat B')
+else:
+    print('Mouse C')
