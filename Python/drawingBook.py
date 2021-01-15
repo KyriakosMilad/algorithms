70e21294d52a7111830c55c0e8a6e6f9599e3377
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-01-15 18:21:35 +0200

    Solve drawingBook.py

--- Python/drawingBook.py
+++ Python/drawingBook.py
@@ -1,1 +1,6 @@
 # challenge from hackerrank https://www.hackerrank.com/challenges/drawing-book
+
+n = int(input('Enter total book pages: '))
+p = int(input('Enter wanted page: '))
+
+print(min(p // 2, n // 2 - p // 2))
