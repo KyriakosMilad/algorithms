86d56f2c5dce2a67b96cbd4b15b0a13f787d30b6
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-01-18 20:34:00 +0200

    Solve beautifulDaysAtTheMovies.py

--- Python/beautifulDaysAtTheMovies.py
+++ Python/beautifulDaysAtTheMovies.py
@@ -1,1 +1,24 @@
 # challenge from hackerrank https://www.hackerrank.com/challenges/beautiful-days-at-the-movies
+
+def get_the_reverse(integer):
+    string = str(integer)
+
+    maximum = len(string)
+    new_string = ''
+
+    for digit in range(maximum - 1, -1, -1):
+        new_string += string[digit]
+
+    return int(new_string)
+
+
+i = int(input('Enter the starting day number: '))
+j = int(input('Enter the ending day number: '))
+k = int(input('Enter the divisor: '))
+
+counter = 0
+for day in range(i, j + 1):
+    if abs(day - get_the_reverse(day)) % k == 0:
+        counter += 1
+
+print(counter)
