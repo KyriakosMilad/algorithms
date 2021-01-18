90304c949ba0af7ec95833b333f55583b67dd2d9
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-01-18 17:57:29 +0200

    Solve findDigits.py

--- Python/findDigits.py
+++ Python/findDigits.py
@@ -1,1 +1,12 @@
 # challenge from hackerrank https://www.hackerrank.com/challenges/find-digits
+
+n = input('Enter the number: ')
+n_integer = int(n)
+
+total = 0
+
+for i in n:
+    if int(i) != 0 and n_integer % int(i) == 0:
+        total += 1
+
+print(total)
