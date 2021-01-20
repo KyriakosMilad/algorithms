66004306a20753342c20ae7a33d56f2bb6d752c6
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-01-20 15:30:18 +0200

    Solve strangeAdvertising.py

--- Python/strangeAdvertising.py
+++ Python/strangeAdvertising.py
@@ -1,2 +1,12 @@
 # challenge from hackerrank https://www.hackerrank.com/challenges/strange-advertising
 
+n = int(input('Enter the day number to report: '))
+
+total = 0
+people = 5
+for i in range(n):
+    new_people = people // 2
+    total += new_people
+    people = new_people * 3
+
+print(total)
