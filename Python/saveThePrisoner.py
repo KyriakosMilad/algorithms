730ba222fd994e89f592c955dd695139e58246e3
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-01-22 20:31:30 +0200

    Solve saveThePrisoner.py

--- Python/saveThePrisoner.py
+++ Python/saveThePrisoner.py
@@ -1,1 +1,16 @@
 # challenge from hackerrank https://www.hackerrank.com/challenges/save-the-prisoner
+
+n = int(input('Enter the number of prisoners: '))
+m = int(input('Enter the number of sweets: '))
+s = int(input('Enter the chair number to begin passing out sweets from: '))
+
+counter = 0
+chair = s
+
+for i in range(1, m):
+    if chair == n:
+        chair = 1
+    else:
+        chair += 1
+
+print(chair)
