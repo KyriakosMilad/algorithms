0167d859e2faab2101fa6d977c98f48827b103da
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-01-28 00:03:18 +0200

    Solve cutTheSticks.py

--- Python/cutTheSticks.py
+++ Python/cutTheSticks.py
@@ -1,1 +1,24 @@
 # challenge from hackerrank https://www.hackerrank.com/challenges/cut-the-sticks
+
+def int_list(item):
+    return int(item)
+
+
+arr = map(int_list, input('Enter elements each element separated with ",": ').split(','))
+arr = list(arr)
+
+answer = []
+
+while True:
+    arr = [value for value in arr if value != 0]
+
+    if len(arr) == 0:
+        break
+
+    shortest = min(arr)
+    answer.append(len(arr))
+
+    for idx, val in enumerate(arr):
+        arr[idx] = val - shortest
+
+print(answer)
