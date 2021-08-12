a083d2adf40dbbe875e86c1f6f2751f932f2d868
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-08-12 07:21:41 +0200

    Solve selectionSort.py

--- Python/selectionSort.py
+++ Python/selectionSort.py
@@ -1,1 +1,16 @@
 # given array, sort this array in selection sort way
+
+def selection_sort(a):
+    for i in range(len(a)):
+        lowest = i
+        for j in range(i + 1, len(a)):
+            if a[j] < a[lowest]:
+                lowest = j
+        if lowest != i:
+            temp = a[i]
+            a[i] = a[lowest]
+            a[lowest] = temp
+    print(a)
+
+
+selection_sort([0, 2, 34, 22, 10, 19, 17])
