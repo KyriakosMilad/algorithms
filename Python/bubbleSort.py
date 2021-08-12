a651afe110f2ecf2846d91ddd57c45897b60be68
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-08-12 06:01:22 +0200

    Solve bubbleSort.py

--- Python/bubbleSort.py
+++ Python/bubbleSort.py
@@ -1,1 +1,18 @@
 # challenge from hackerrank https://www.hackerrank.com/challenges/ctci-bubble-sort
+
+def bubble_sort(a):
+    count = 0
+    for i in a:
+        current_count = count
+        for j in range(len(a) - 1):
+            if a[j] > a[j + 1]:
+                temp = a[j]
+                a[j] = a[j + 1]
+                a[j + 1] = temp
+                count += 1
+        # check if the array sorted stop the loop
+        if current_count == count:
+            break
+    print("Array is sorted in " + str(count) + " swaps.")
+    print("First Element: " + str(a[0]))
+    print("Last Element: " + str(a[-1]))
