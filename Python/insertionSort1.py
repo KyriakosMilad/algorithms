8162df00bf4ad2cb437f955a9c04fd863ac49b3d
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-08-13 07:43:32 +0200

    Solve insertionSort1.py

--- Python/insertionSort1.py
+++ Python/insertionSort1.py
@@ -1,1 +1,16 @@
 # challenge from hackerrank https://www.hackerrank.com/challenges/insertionsort1
+
+def insertion_sort1(arr_size, arr):
+    current_value = arr[arr_size - 1]
+    for j in range(arr_size - 2, -1, -1):
+        # print(j, arr[j], arr[j+1])
+        if current_value < arr[j]:
+            arr[j + 1] = arr[j]
+            print(*arr)
+            if j == 0:
+                arr[0] = current_value
+                print(*arr)
+        elif current_value >= arr[j]:
+            arr[j + 1] = current_value
+            print(*arr)
+            break
