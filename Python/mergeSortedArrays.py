a61b7871bec83bac5f6b66b7a642e7f94d1ab586
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-08-16 04:46:29 +0200

    Solve mergeSortedArrays.py

--- Python/mergeSortedArrays.py
+++ Python/mergeSortedArrays.py
@@ -1,1 +1,26 @@
 # given two arrays, merge those two arrays into one sorted array
+
+def merge_sorted_arrays(arr1, arr2):
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            result.append(arr1[i])
+            i += 1
+        else:
+            result.append(arr2[j])
+            j += 1
+
+    while i < len(arr1):
+        result.append(arr1[i])
+        i += 1
+    while j < len(arr2):
+        result.append(arr2[j])
+        j += 1
+
+    print(result)
+
+
+merge_sorted_arrays([60], [4, 100, 3])
